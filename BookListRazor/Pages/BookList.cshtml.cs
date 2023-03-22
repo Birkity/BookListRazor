dfc2b176b46a5d2a0e5eb6f43c15b3d034a11233
Birkity <lilyBluestorm7@gmail.com>
2023-03-22 02:09:29 -0700

    ADD DATA to DB and create display table for book

---
+++ BookListRazor/Pages/BookList.cshtml.cs
@@ -0,0 +1,23 @@
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+    }
+}

e615b97c3eb620b3257c80f66402c9bb8e967eb5
Birkity <lilyBluestorm7@gmail.com>
2023-03-23 20:51:35 -0700

    add new column ISBN to the database

--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -10,5 +10,7 @@ namespace BookListRazor.Model
         public string Name { get; set; }
         public string Author { get; set; }
 
+        public string ISBN { get; set; }
+
     }
 }
---
+++ BookListRazor/Pages/Create.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Book Book { get; set; }
+        public void OnGet() 
+        { 
+        
+
+        }
+    }
+}

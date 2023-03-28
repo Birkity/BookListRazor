64fb902469d3decaa5e1add6a116608612a9c38c
Birkity <lilyBluestorm7@gmail.com>
2023-03-28 09:37:38 -0700

    add API and Controller and minor edits

---
+++ BookListRazor/Controller/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookListRazor.Controller
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
